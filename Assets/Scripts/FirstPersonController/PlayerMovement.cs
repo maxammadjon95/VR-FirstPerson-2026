a1022febb295601ab31ef050c988f8162ef18f70
Makhammadjon <mr.mahammadjon@mail.ru>
2026-09-16 20:28:24 +0500

    Update PlayerMovement.cs

--- Assets/Scripts/FirstPersonController/PlayerMovement.cs
+++ Assets/Scripts/FirstPersonController/PlayerMovement.cs
@@ -5,15 +5,15 @@ namespace Scratch.FirstPerson
     [RequireComponent(typeof(CharacterController))]
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField] CharacterController controller;
-        [SerializeField] PlayerJump jump;
-        [SerializeField] float moveSpeed = 5f;
+        [SerializeField] CharacterController _controller;
+        [SerializeField] PlayerJump _jump;
+        [SerializeField] float _moveSpeed = 5f;
 
         public void Move(Vector2 input, float deltaTime)
         {
-            Vector3 horizontal = (transform.right * input.x + transform.forward * input.y) * moveSpeed;
-            Vector3 velocity = horizontal + Vector3.up * jump.VerticalVelocity;
-            controller.Move(velocity * deltaTime);
+            Vector3 horizontal = (transform.right * input.x + transform.forward * input.y) * _moveSpeed;
+            Vector3 velocity = horizontal + Vector3.up * _jump.VerticalVelocity;
+            _controller.Move(velocity * deltaTime);
         }
     }
 }
